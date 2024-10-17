54e635011ce9cd01313a9a5ffc9f0c7c2490d301
RelictRLCT <pigift@yandex.ru>
2024-10-17 21:10:17 +0300

    Рефакторинг

--- lab2/show.py
+++ lab2/show.py
@@ -3,7 +3,10 @@ from automata.fa.nfa import NFA
 from matplotlib import pyplot as plt
 from typing import Union
 
+from pathlib import Path
+
 def show(automata: Union[DFA, NFA]):
+    Path("images").mkdir(parents=True, exist_ok=True)
     automata.show_diagram().draw('images/labyrinth.png', prog='dot')
     img = plt.imread('images/labyrinth.png')
     plt.imshow(img)
