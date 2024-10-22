8ba85329958d36f3f5f311d1e6933161480dee15
RelictRLCT <pigift@yandex.ru>
2024-10-22 14:00:03 +0300

    Изменён формат ответов, добавлена проверка вложенности языка лабиринта в язык пользователя

--- lab2tfl/member_equal.py
+++ lab2tfl/member_equal.py
@@ -29,39 +29,3 @@ def equal_labyrinths(labyrinth: DFA, learner_dfa: DFA) -> (str, bool):
 
 def membership(labyrinth: DFA, word: str) -> bool:
     return labyrinth.accepts_input(word)
-
-
-if __name__ == "__main__":
-    dfa1 = DFA(
-        states={'e', 'a', 'b', 'ba', 'bb'},
-        input_symbols={'a', 'b'},
-        transitions={
-            'e': {'b': 'b', 'a': 'a'},
-            'a': {'a': 'e', 'b': 'ba'},
-            'b': {'a': 'ba', 'b': 'bb'},
-            'ba': {'a': 'b', 'b': 'bb'},
-            'bb': {'a': 'bb', 'b': 'bb'}
-        },
-        initial_state='e',
-        final_states={'b'},
-        allow_partial=True
-    )
-
-    show(dfa1)
-
-    dfa2 = DFA(
-        states={'q1', 'q2', 'trap'},
-        input_symbols={'a', 'b'},
-        transitions={
-            'q1': {'b': 'q2', 'a': 'trap'},
-            'trap': {'a': 'trap', 'b': 'trap'},
-            'q2': {'a': 'trap', 'b': 'trap'}
-        },
-        initial_state='q1',
-        final_states={'q2'},
-        allow_partial=True
-    )
-
-    show(dfa2)
-
-    print(equal_labyrinths(dfa1, dfa2))
