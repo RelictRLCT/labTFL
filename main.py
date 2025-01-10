a6b27e783a495318bf8ee2361ae18eb8bdfe8ddc
RELICT <pigift@yandex.ru>
2025-01-10 19:19:28 +0300

    Добавлены тесты и два режима работы: с прогоном тестов и с анализом одного регекса, обновлен README

--- main.py
+++ main.py
@@ -6,12 +6,7 @@ from src.validator.syntax_validator import validate_syntax
 from src.visualise.visualizer import visualize_tree
 
 
-def main():
-    with open('regex.txt', 'r') as f:
-        grammar = f.readline().strip()
-        print(f"Переданный регекс: {grammar}")
-
-    #grammar = "(a|(bb))(a|(?6()))"
+def check_grammar(grammar: str, show: bool = True):
     is_valid, msg = validate_syntax(grammar)
     if not is_valid:
         print("Синтаксис регекса некорректен.", msg)
@@ -19,9 +14,10 @@ def main():
 
     is_valid, tree = validate_regex(grammar)
 
-    graph = Digraph(format="png")
-    visualize_tree(tree, graph)
-    graph.render("tree_view", view=False)
+    if show:
+        graph = Digraph(format="png")
+        visualize_tree(tree, graph)
+        graph.render("tree_view", view=False)
 
     if is_valid:
         print("Регекс корректен")
@@ -37,6 +33,42 @@ def main():
                 print(f"{nt} -> {' | '.join(prod_strings)}")
     else:
         print("Регекс некорректен")
+
+
+def main():
+    tests = int(
+                input(
+                    "Выберите режим: \n1 - проверка регекса из файла regex.txt\n"
+                    "2 - автоматический прогон тестов из файлов tests_correct.txt "
+                    "и tests_incorrect.txt\n"
+                )
+            )
+    match tests:
+        case 1:
+            with open('regex.txt', 'r') as f:
+                grammar = f.readline().strip()
+                print(f"Переданный регекс: {grammar}")
+            check_grammar(grammar)
+        case 2:
+            print("\nКорректные тесты:\n")
+            with open('tests_correct.txt', 'r') as f:
+                for grammar in f:
+                    grammar = grammar.strip()
+                    if grammar:
+                        print(f"Переданный регекс: {grammar}")
+                        check_grammar(grammar, show=False)
+                        print()
+
+            print("\nНекорректные тесты:\n")
+            with open('tests_incorrect.txt', 'r') as f:
+                for grammar in f:
+                    grammar = grammar.strip()
+                    if grammar:
+                        print(f"Переданный регекс: {grammar}")
+                        check_grammar(grammar, show=False)
+                        print()
+        case _:
+            print("Неверный режим")
 
 
 if __name__ == "__main__":
